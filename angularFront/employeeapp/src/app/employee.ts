e536639ee8a7d95b5792969273511fb58e1de50a
Kehinde Adeoye <t2dstudio@yahoo.com>
2021-08-26 21:33:04 -0500

    Injecting services

--- angularFront/employeeapp/src/app/employee.ts
+++ angularFront/employeeapp/src/app/employee.ts
@@ -1,4 +1,4 @@
-export interface Employee {
+export class Employee {
 id: number;
 name: string;
 email: string;
@@ -6,5 +6,15 @@ jobTitle: string;
 phone: string;
 imageUrl: string;
 employeeCode: string;
+
+constructor(id =0, name="", jobTitle = "", email="", phone="",imageUrl="",employeeCode=""){
+    this.id= id;
+    this.name = name;
+    this.jobTitle = jobTitle;
+    this.phone = phone;
+    this.email = email;
+    this.imageUrl = imageUrl;
+    this.employeeCode = employeeCode;
+}
 
 }
--- src/main/java/com/t2dstudio/admin/controller/EmployeeResource.java
+++ src/main/java/com/t2dstudio/admin/controller/EmployeeResource.java
@@ -2,9 +2,9 @@ package com.t2dstudio.admin.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +19,7 @@ import com.t2dstudio.admin.service.EmployeeService;
 
 @RestController
 @RequestMapping("/employee")
+@CrossOrigin(origins = "*")
 public class EmployeeResource {
 	
 	
